fbaad9598ece37c1656bb6c6777475940ef3c3cb
Mohamed <mhassankd35@gmail.com>
2023-01-24 16:52:34 -0600

    adds app stack to conditional rendering

--- App.tsx
+++ App.tsx
@@ -2,17 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./screens/Login";
-import Register from "./screens/Register";
+import Login from "./screens/LoginScreen";
+import Register from "./screens/RegisterScreen";
 import { AuthStack } from "./navigators/AuthStack";
+import AppStack from "./navigators/AppStack";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const user = false;
+  const user = true;
   return (
     <NavigationContainer>
-      {user ? <Text>signed in</Text> : <AuthStack />}
+      {user ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
 }
