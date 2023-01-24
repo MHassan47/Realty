8cab1e649f52be8913394eb4c03216d93d32b9ee
Mohamed <mhassankd35@gmail.com>
2023-01-24 16:31:16 -0600

    refactors app to conditionally render auth or home screen

--- App.tsx
+++ App.tsx
@@ -4,16 +4,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
+import { AuthStack } from "./navigators/AuthStack";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = false;
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
+      {user ? <Text>signed in</Text> : <AuthStack />}
     </NavigationContainer>
   );
 }
