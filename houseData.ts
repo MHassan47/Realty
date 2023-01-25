2d9cbd213b5aa214471c6aeadc3542ea0c1ffd44
Mohamed <mhassankd35@gmail.com>
2023-01-25 11:49:21 -0600

    adds components to tailwind config

---
+++ houseData.ts
@@ -0,0 +1,56 @@
+export const houses = [
+  {
+    id: "1",
+    title: "first house",
+    image:
+      "https://photos.zillowstatic.com/fp/c73ec60a73c5ffdeceaf55119ff1d811-p_e.jpg",
+    price: 276000,
+    location: "Seattle, WA",
+    rooms: { bedrooms: 4, bathrooms: 3 },
+  },
+  {
+    id: "2",
+    title: "second house",
+    image:
+      "https://photos.zillowstatic.com/fp/75d031fc60d2b36f716b4d0bdc1bd802-p_e.jpg",
+    price: 399000,
+    location: "Vancouver, BC",
+    rooms: { bedrooms: 4, bathrooms: 3 },
+  },
+  {
+    id: "3",
+    title: "third house",
+    image:
+      "https://photos.zillowstatic.com/fp/89c2d1714755956b107432fd8430936c-p_e.jpg",
+    price: 270000,
+    location: "Toronto, ON",
+    rooms: { bedrooms: 5, bathrooms: 4 },
+  },
+  {
+    id: "4",
+    title: "fourth house",
+    image:
+      "https://photos.zillowstatic.com/fp/4f8d35583e0493ab534b480c5ec09e02-p_e.jpg",
+    price: 392000,
+    location: "Houston, TX",
+    rooms: { bedrooms: 4, bathrooms: 2 },
+  },
+  {
+    id: "5",
+    title: "fifth house",
+    image:
+      "https://photos.zillowstatic.com/fp/1d30f88e86d10b3712a43534920396a3-p_e.jpg",
+    price: 560000,
+    location: "Atlanta, GA",
+    rooms: { bedrooms: 3, bathrooms: 3 },
+  },
+  {
+    id: "6",
+    title: "sixth house",
+    image:
+      "https://photos.zillowstatic.com/fp/a8a1af5d0450ca83873afcae58471015-cc_ft_1536.webp",
+    price: 295000,
+    location: "Minneapolis, MN",
+    rooms: { bedrooms: 4, bathrooms: 3 },
+  },
+];
