042f04c9ede83265232138d4bd02527477c8d016
Mohamed <mhassankd35@gmail.com>
2023-01-24 20:07:31 -0600

    creates user slice logic

---
+++ redux/userSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+
+// Define a type for the slice state
+interface UserSlice {
+  user: User;
+}
+interface User {
+  id: string | null;
+  name: string | null;
+  email: string | null;
+}
+
+// Define the initial state using that type
+const initialState: UserSlice = {
+  user: { id: null, name: null, email: null },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+      console.log(state.user);
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user.user;
+export default userSlice.reducer;
