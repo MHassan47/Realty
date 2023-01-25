946864626a52d78372b22d69839e29a4dd262acd
Mohamed <mhassankd35@gmail.com>
2023-01-25 11:50:50 -0600

    creates bottom tab style app navigation

--- navigators/AppStack.tsx
+++ navigators/AppStack.tsx
@@ -1,12 +1,33 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
-
+import { AntDesign, Ionicons, EvilIcons } from "@expo/vector-icons";
+import ProfileScreen from "../screens/ProfileScreen";
 const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home";
+            return <AntDesign name={"home"} size={size} color={color} />;
+          } else if (route.name === "Profile") {
+            return <AntDesign name={"user"} size={size} color={color} />;
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#437370",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
