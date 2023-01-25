f0a96f5b7cfe074876cc4323c523738d57b09a0c
Mohamed <mhassankd35@gmail.com>
2023-01-25 14:13:37 -0600

    adds longitude, latitude, cars and description to house data

--- houseData.ts
+++ houseData.ts
@@ -5,8 +5,15 @@ export const houses = [
     image:
       "https://photos.zillowstatic.com/fp/c73ec60a73c5ffdeceaf55119ff1d811-p_e.jpg",
     price: 276000,
+    address: "1623 Myrtle Drive",
     location: "Seattle, WA",
+    longitude: -122.313302,
+    latitude: 47.603385,
     rooms: { bedrooms: 4, bathrooms: 3 },
+    size: 1450,
+    cars: 3,
+    description:
+      "Gorgeous 4 bed, 3 bath half-duplex home with spacious living area and great windows. Large Kitchen has ceramic tile, gorgeous granite counters, wood cabinets, vaulted ceilings and breakfast bar. Master bath features Jetted Tub, separate shower and bath.",
   },
   {
     id: "2",
@@ -14,8 +21,15 @@ export const houses = [
     image:
       "https://photos.zillowstatic.com/fp/75d031fc60d2b36f716b4d0bdc1bd802-p_e.jpg",
     price: 399000,
+    address: "1915 Juniper Drive",
     location: "Vancouver, BC",
+    longitude: -123.081471,
+    latitude: 49.235422,
     rooms: { bedrooms: 4, bathrooms: 3 },
+    size: 1745,
+    cars: 3,
+    description:
+      "Expansive two-story home in Woodlake West that is turnkey - just move in and enjoy! The interior boasts fresh paint throughout in neutral colors, easy care hard surface floors on the main level and brand new carpeting upstairs. The living room is incredibly spacious and filled with natural light creating a wonderful space for entertaining. Fully equipped kitchen features extensive cabinet space, an adjacent dining area and a breakfast bar that provides additional seating and opens out to the living room allowing the chef to feel connected to the action.",
   },
   {
     id: "3",
@@ -23,8 +37,15 @@ export const houses = [
     image:
       "https://photos.zillowstatic.com/fp/89c2d1714755956b107432fd8430936c-p_e.jpg",
     price: 270000,
+    address: "2012 Lake Mose Lane",
     location: "Toronto, ON",
-    rooms: { bedrooms: 5, bathrooms: 4 },
+    longitude: -79.508659,
+    latitude: 43.766135,
+    rooms: { bedrooms: 3, bathrooms: 3 },
+    size: 1130,
+    cars: 3,
+    description:
+      "Gorgeous 3 bed, 3 bath half-duplex home with spacious living area and great windows. Large Kitchen has ceramic tile, gorgeous granite counters, wood cabinets, vaulted ceilings and breakfast bar. Master bath features Jetted Tub, separate shower and bath.",
   },
   {
     id: "4",
@@ -32,8 +53,15 @@ export const houses = [
     image:
       "https://photos.zillowstatic.com/fp/4f8d35583e0493ab534b480c5ec09e02-p_e.jpg",
     price: 392000,
+    address: "55 Pinecove Street",
     location: "Houston, TX",
+    longitude: -95.350971,
+    latitude: 29.71191,
     rooms: { bedrooms: 4, bathrooms: 2 },
+    size: 1380,
+    cars: 3,
+    description:
+      "Gorgeous 4 bed, 2 bath half-duplex home with spacious living area and great windows. Large Kitchen has ceramic tile, gorgeous granite counters, wood cabinets, vaulted ceilings and breakfast bar. Master bath features Jetted Tub, separate shower and bath.",
   },
   {
     id: "5",
@@ -41,8 +69,15 @@ export const houses = [
     image:
       "https://photos.zillowstatic.com/fp/1d30f88e86d10b3712a43534920396a3-p_e.jpg",
     price: 560000,
+    address: "103 Bird Creek Drive",
     location: "Atlanta, GA",
-    rooms: { bedrooms: 3, bathrooms: 3 },
+    longitude: -84.386244,
+    latitude: 33.75974,
+    rooms: { bedrooms: 5, bathrooms: 3 },
+    size: 1825,
+    cars: 3,
+    description:
+      "Great location in Bird Creek, just off the lake, on a large corner lot! 5 Bed, 3 Bath home located in the desirable Bird Creek community. Gorgeous saltwater pool with spa and slide. Wrought iron fence divides swim and play space. Master down along with 1.5 baths. 4 beds up, game room and media room.",
   },
   {
     id: "6",
@@ -50,7 +85,14 @@ export const houses = [
     image:
       "https://photos.zillowstatic.com/fp/a8a1af5d0450ca83873afcae58471015-cc_ft_1536.webp",
     price: 295000,
+    address: "1120 Lasso Street",
     location: "Minneapolis, MN",
+    longitude: -93.25985,
+    latitude: 44.958968,
     rooms: { bedrooms: 4, bathrooms: 3 },
+    size: 1325,
+    cars: 3,
+    description:
+      "Gorgeous 4 bed, 3 bath half-duplex home with spacious living area and great windows. Large Kitchen has ceramic tile, gorgeous granite counters, wood cabinets, vaulted ceilings and breakfast bar. Master bath features Jetted Tub, separate shower and bath.",
   },
 ];
