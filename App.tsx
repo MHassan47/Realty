2ac1f9d7ef7f82f46984b123188a76bf2f1984a4
Mohamed <mhassankd35@gmail.com>
2023-02-02 21:22:30 -0600

    adds an error handling

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import Routes from "./Routes";
-
+import { LogBox } from "react-native";
+LogBox.ignoreLogs([
+  "Warning: Async Storage has been extracted from react-native core",
+]);
 export default function App() {
   return (
     <Provider store={store}>
