c48fbe93da42997a81f19bfa5fff85e3bc09d1be
Mohamed <mhassankd35@gmail.com>
2023-02-02 21:22:46 -0600

    removes user name header

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -60,7 +60,6 @@ const Home = () => {
 
   return (
     <SafeAreaView className="bg-gray-100 flex-1 items-center">
-      <Text className="text-lg font-semibold">{user.name}</Text>
       <View className="flex-row w-4/5 items-center space-x-2 p-2 rounded-lg">
         <View className="flex-row bg-gray-200 p-3 space-x-3 rounded-xl shadow-sm">
           <AntDesign name="search1" color="gray" size={20} />
